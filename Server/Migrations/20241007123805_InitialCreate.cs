251ad5c8912084045d84f28f4ac7bfd4e766b05d
Lucas Meijer <lucas@unity3d.com>
2024-10-07 14:38:34 +0200

    initial migration

---
+++ Server/Migrations/20241007123805_InitialCreate.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolidGround.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Executions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StartTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Executions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inputs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inputs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InputComponents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    InputId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    StringValue = table.Column<string>(type: "TEXT", nullable: false),
+                    BinaryValue = table.Column<byte[]>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InputComponents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InputComponents_Inputs_InputId",
+                        column: x => x.InputId,
+                        principalTable: "Inputs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Outputs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExecutionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InputId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outputs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Outputs_Executions_ExecutionId",
+                        column: x => x.ExecutionId,
+                        principalTable: "Executions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Outputs_Inputs_InputId",
+                        column: x => x.InputId,
+                        principalTable: "Inputs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutputComponents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OutputId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutputComponents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OutputComponents_Outputs_OutputId",
+                        column: x => x.OutputId,
+                        principalTable: "Outputs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InputComponents_InputId",
+                table: "InputComponents",
+                column: "InputId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutputComponents_OutputId",
+                table: "OutputComponents",
+                column: "OutputId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Outputs_ExecutionId",
+                table: "Outputs",
+                column: "ExecutionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Outputs_InputId",
+                table: "Outputs",
+                column: "InputId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InputComponents");
+
+            migrationBuilder.DropTable(
+                name: "OutputComponents");
+
+            migrationBuilder.DropTable(
+                name: "Outputs");
+
+            migrationBuilder.DropTable(
+                name: "Executions");
+
+            migrationBuilder.DropTable(
+                name: "Inputs");
+        }
+    }
+}
